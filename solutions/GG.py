d62a15ead15317e1118d3bf5c1a26f09f93d2313
fedalegend <fparhomenko79@gmail.com>
2026-07-28 15:59:13 +0300

    complete a solution of G problem

--- solutions/GG.py
+++ solutions/GG.py
@@ -9,5 +9,19 @@ for i in range(n):
     if i == k - 1:
         b.append(cur)
     if i >= k:
-        cur += a[i]
-        
+        cur += (a[i] - a[i - k])
+        b.append(cur)
+
+left = [b[0]]
+right = [b[-1]]
+for i in range(1, len(b)):
+    left.append(max(left[-1], b[i]))
+for i in range(len(b) - 2, -1, -1):
+    right.append(max(right[-1], b[i]))
+right.reverse()
+
+res = 0
+for i in range(len(b) - k):
+    res = max(res, left[i] + right[i + k])
+
+print(res)
