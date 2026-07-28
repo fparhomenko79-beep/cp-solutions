003c994ca160816fcdbf3a926c68ac8235686911
fedalegend <fparhomenko79@gmail.com>
2026-07-28 17:26:12 +0300

    add a solution of j problem

--- solutions/JJ.py
+++ solutions/JJ.py
@@ -1,13 +1,22 @@
+import math
 
+n, x, y = map(int, input().split())
 
-urls = [
-    "http://az.lib.ru/t/tolstoj_lew_nikolaewich/text_0039.shtml",
-    "http://az.lib.ru/t/tolstoj_lew_nikolaewich/text_0040.shtml",
-    "http://az.lib.ru/t/tolstoj_lew_nikolaewich/text_0050.shtml",
-    "http://az.lib.ru/t/tolstoj_lew_nikolaewich/text_0060.shtml",
-    "http://az.lib.ru/t/tolstoj_lew_nikolaewich/text_0070.shtml",
-    "http://az.lib.ru/t/tolstoj_lew_nikolaewich/text_0080.shtml",
-    "http://az.lib.ru/t/tolstoj_lew_nikolaewich/text_0090.shtml",
-    "http://az.lib.ru/t/tolstoj_lew_nikolaewich/text_1860_dekabristy.shtml",
-]
+nt = min(x, y) + math.ceil((n - 1) / (1/x + 1/y))
+print(nt)
 
+# def check(t, x, y, n):
+#
+#     return nt <= t
+#
+# res = float("inf")
+# l, r = 0, 10**18
+# while l < r:
+#     mid = (l + r) // 2
+#     if check(mid, x, y, n):
+#         r = mid - 1
+#         res = min(res, mid)
+#     else:
+#         l = mid + 1
+#
+# print(res)
