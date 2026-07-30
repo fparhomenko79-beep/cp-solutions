0d392eff3b3b0bd5e99fdee2175315f6e2a72c77
fedalegend <fparhomenko79@gmail.com>
2026-07-30 08:42:53 +0300

    add a solution of R probleem and fix Q

--- solutions/RR.py
+++ solutions/RR.py
@@ -4,8 +4,8 @@ dp = [0] * (n + 1)
 
 for i in range(1, n + 1):
     if i >= 2:
-        dp[i] += min(dp[i - 1], dp[i - 2]) + a[i - 1]
+        dp[i] += max(dp[i - 1], dp[i - 2]) + a[i - 1]
     else:
         dp[i] = a[i - 1]
 
-print(dp[n])
+print(dp[-1])
