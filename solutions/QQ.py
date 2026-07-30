a8cc3cb17d0c51795bf6edf9122bec2753e159f6
fedalegend <fparhomenko79@gmail.com>
2026-07-30 08:37:31 +0300

    add a solution of Q problem

---
+++ solutions/QQ.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = list(map(int, input().split()))
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    if i >= 2:
+        dp[i] += min(dp[i - 1], dp[i - 2]) + a[i - 1]
+    else:
+        dp[i] = a[i]
+
+print(dp[n])
