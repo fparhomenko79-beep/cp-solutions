62d0fadd41850718e1b5981942abb1494e47eb19
fedalegend <fparhomenko79@gmail.com>
2026-08-02 09:41:43 +0300

    =)

--- digit dp.py
+++ digit dp.py
@@ -1,35 +1,43 @@
 q = int(input())
+
 for _ in range(q):
     a, b = input().split()
-    N = len(b)
-    a.zfill(N)
+    a = a.zfill(len(b))
     a = list(map(int, a))
     b = list(map(int, b))
 
-    dp = [[-1] * 2 for _ in range(N)]
-    dp[1][1] = 0
+    dp = {(1, 1): 0}
 
-    for i in range(N):
-        A = int(a[i])
-        B = int(b[i])
+    for i in range(len(a)):
+        new = {}
 
-        new = [[-1] * 2 for _ in range(N)]
+        for (low, high), current_sum in dp.items():
+            if low:
+                left_num = a[i]
+            else:
+                left_num = 0
+            if high:
+                right_num = b[i]
+            else:
+                right_num = 9
 
-        for low in range(2):
-            for high in range(2):
-                if dp[low][high] == -1:
-                    continue
+            for j in range(left_num, right_num + 1):
+                new_low = 1 if (low and j == a[i]) else 0
+                new_high = 1 if (high and j == b[i]) else 0
 
-                min_d = A if low else 0
-                max_d = B if high else 9
+                new_sum = current_sum + j
 
-                for d in range(min_d. max_d + 1):
-                    nxt_low = 1 if (low and d == A) else 0
-                    nxt_high = 1 if (high and d == B) else 0
+                sost = (new_low, new_high)
 
-                    if dp[low][high] + d > new[nxt_low][nxt_high]:
-                        new[nxt_low][nxt_high] = dp[low][high] + d
+                if sost not in new:
+                    new[sost] = new_sum
+                else:
+                    new[sost] = max(new[sost], new_sum)
+
         dp = new
 
-    print(max(dp[0][0], dp[0][1], dp[1][0], dp[1][1]))
-    
+    ans = 0
+    for current_sum in dp.values():
+        ans = max(ans, current_sum)
+
+    print(ans)
---
+++ solutions/digit dp B.py
@@ -0,0 +1,61 @@
+q = int(input())
+
+for _ in range(q):
+    a, b = input().split()
+    a = a.zfill(len(b))
+    a = list(map(int, a))
+    b = list(map(int, b))
+
+    dp = {(1, 1, 0, 0): 0}
+
+    for i in range(len(a)):
+        new = {}
+
+        for (low, high, started, is_even_pos), score in dp.items():
+            if low:
+                left_num = a[i]
+            else:
+                left_num = 0
+
+            if high:
+                right_num = b[i]
+            else:
+                right_num = 9
+
+            for j in range(left_num, right_num + 1):
+                new_low = low and (j == a[i])
+                new_high = high and (j == b[i])
+
+                if started:
+                    new_started = 1
+                    if is_even_pos and (j % 2 == 0):
+                        new_score = score + 1
+                    else:
+                        new_score = score
+
+                    new_is_even_pos = 1 - is_even_pos
+                else:
+                    if j == 0:
+                        new_started = 0
+                        new_is_even_pos = 0
+                        new_score = score
+                    else:
+                        new_started = 1
+                        new_is_even_pos = 1
+                        new_score = score
+
+                sost = (new_low, new_high, new_started, new_is_even_pos)
+
+                if sost not in new:
+                    new[sost] = new_score
+                else:
+                    new[sost] = max(new[sost], new_score)
+
+        dp = new
+
+    ans = 0
+
+    for (low, high, started, is_even_pos), score in dp.items():
+        if started:
+            ans = max(ans, score)
+    print(ans)
